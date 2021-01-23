a3dffced829634cbb5743c3a1635ebbf34b17ac1
raveenamnair <nair64@purdue.edu>
2021-01-23 08:23:20 -0800

    Got video uri from chosen video

--- app/src/main/java/com/raveena/snug/UploadingActivity.java
+++ app/src/main/java/com/raveena/snug/UploadingActivity.java
@@ -1,23 +1,37 @@
 package com.raveena.snug;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.core.Constants;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.net.URI;
 
 public class UploadingActivity extends AppCompatActivity {
 
     Button uploadBtn;
+    VideoView videoField;
     private Uri videoUri;
     private static final int REQUEST_CODE = 101;
     private StorageReference videoRef;
@@ -28,6 +42,7 @@ public class UploadingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_uploading);
 
         uploadBtn = findViewById(R.id.uploadBtn);
+        videoField = findViewById(R.id.videoview);
 
         // clicking this button will allow you to choose a video
         uploadBtn.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +58,38 @@ public class UploadingActivity extends AppCompatActivity {
         });
 
     }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            Uri uri = data.getData();
+            try{
+                videoField.setVideoURI(uri);
+                Toast.makeText(getApplicationContext(), getRealPathFromURI(getApplicationContext(), uri), Toast.LENGTH_LONG).show();
 
+                videoField.start();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    // This method lied and doesn't actually work - produces empty string
+    public String getRealPathFromURI(Context context, Uri contentUri) {
+        Cursor cursor = null;
+        try {
+            String[] proj = { MediaStore.Video.Media.DATA };
+            cursor = context.getContentResolver().query(contentUri,  proj, null, null, null);
+            int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        } finally {
+            if (cursor != null) {
+                cursor.close();
+            }
+        }
+    }
 
 
 
