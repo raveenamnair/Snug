5636b391ca3e05823541f5b1172185f589f274ac
raveenamnair <nair64@purdue.edu>
2021-01-23 05:40:55 -0800

    Created Uploading Activity Page

---
+++ app/src/main/java/com/raveena/snug/UploadingActivity.java
@@ -0,0 +1,14 @@
+package com.raveena.snug;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_uploading);
+    }
+}
