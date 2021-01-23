683c4e4945f9c49b2827031baef9aea02970497b
Kristy <kristy.huang15@gmail.com>
2021-01-23 11:25:37 -0500

    uploading

--- app/src/main/java/com/raveena/snug/UploadingActivity.java
+++ app/src/main/java/com/raveena/snug/UploadingActivity.java
@@ -2,18 +2,23 @@ package com.raveena.snug;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 
 import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.core.Constants;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
 
 public class UploadingActivity extends AppCompatActivity {
 
@@ -21,6 +26,9 @@ public class UploadingActivity extends AppCompatActivity {
     private Uri videoUri;
     private static final int REQUEST_CODE = 101;
     private StorageReference videoRef;
+    private DatabaseReference reference;
+    private FirebaseUser firebaseUser;
+    private StorageTask uploadTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,12 +47,20 @@ public class UploadingActivity extends AppCompatActivity {
                 startActivityForResult(intent, 1);
 
                 // TODO Get the filepath of the video to upload to Firebase
+                videoRef = FirebaseStorage.getInstance().getReference("uploads");
+
             }
         });
-
     }
 
 
-
+//    //upload video filepath to firebase storage
+//    private void UploadVideo() {
+//        //TODO: progress bar?
+//        if (videoUri != null) {
+//            final StorageReference fileReference = videoRef.child();
+//            uploadTask = fileReference.putFile()
+//        }
+//    }
 
 }
