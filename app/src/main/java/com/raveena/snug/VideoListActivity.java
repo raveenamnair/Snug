5dbb082780cd1055bbc6a9b0c6dd149add816f41
Kristy <kristy.huang15@gmail.com>
2021-01-23 22:00:38 -0500

    updated uploading and viewing of videos

--- app/src/main/java/com/raveena/snug/VideoListActivity.java
+++ app/src/main/java/com/raveena/snug/VideoListActivity.java
@@ -38,8 +38,7 @@ public class VideoListActivity extends AppCompatActivity {
     String videoPath;
     Button refreshBtn;
     String videoCategory;
-    ArrayList <String> l;
-    boolean clicked = false;
+    int counter = 0;
     DatabaseReference reference;
     List<Video> videoList = new ArrayList<>();
 
@@ -58,6 +57,19 @@ public class VideoListActivity extends AppCompatActivity {
             videoCategory = extra.getString("SITUATION_TYPE");
         }
 
+        video.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                if (video.isPlaying()) {
+                    video.pause();
+                }
+                else {
+                    video.start();
+                }
+                return true;
+            }
+        });
+
         // For debugging purposes
         //Toast.makeText(getApplicationContext(), videoCategory, Toast.LENGTH_SHORT).show();
 
@@ -74,29 +86,7 @@ public class VideoListActivity extends AppCompatActivity {
                     }
                 }
                 Collections.shuffle(videoList);
-                videoPath = videoList.get(0).getFilePath();
-                StorageReference storageReference = storage.getReference().child("uploads/" + videoPath);
-                System.out.println("VIDEOPATH " + videoPath);
-                storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-                    @Override
-                    public void onSuccess(Uri uri) {
-                        // Got the download URL for 'users/me/profile.png'
-                        System.out.println(uri);
-                        video.setVideoURI(uri);
-                        video.start();
-                        //video.pause();
-                        Toast.makeText(getApplicationContext(), "Video success", Toast.LENGTH_LONG).show();
-                        System.out.println("REAL LIST");
-                        for (Video v: videoList) {
-                            System.out.println(v.getCategory());
-                        }
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle any errors
-                    }
-                });
+                generateVideo(storage);
             }
 
             @Override
@@ -108,110 +98,38 @@ public class VideoListActivity extends AppCompatActivity {
         refreshBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "Clicked refresh", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Page Refreshed", Toast.LENGTH_SHORT).show();
+                generateVideo(storage);
             }
         });
-
-
-
-//        StorageReference storageReference = null;
-//        List<Video> specialList = videoList;
-//        for (int i = 0; i < specialList.size(); i++) {
-//            storageReference = storage.getReference().child("uploads/" + specialList.get(i).getFilePath());
-//            System.out.println("VIDEOPATH " + specialList.get(i).getFilePath());
-//            storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//                @Override
-//                public void onSuccess(Uri uri) {
-//                    // Got the download URL for 'users/me/profile.png'
-//                    System.out.println(uri);
-//                    video.setVideoURI(uri);
-//                    video.start();
-//                    Toast.makeText(getApplicationContext(), "Video success", Toast.LENGTH_LONG).show();
-//                }
-//            }).addOnFailureListener(new OnFailureListener() {
-//                @Override
-//                public void onFailure(@NonNull Exception exception) {
-//                    // Handle any errors
-//                    Toast.makeText(getApplicationContext(), "Sorry, your requested video couldn't load", Toast.LENGTH_SHORT).show();
-//                }
-//            });
-//
-//            refreshBtn.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    video.stopPlayback();
-//                    clicked = true;
-//                }
-//            });
-//            if (!clicked) {
-//                video.stopPlayback();
-//                break;
-//            }
-//
-//        }
-
-
-//        StorageReference storageReference = storage.getReference().child("uploads/" + videoPath);
-//        System.out.println("VIDEOPATH " + videoPath);
-//        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//            @Override
-//            public void onSuccess(Uri uri) {
-//                // Got the download URL for 'users/me/profile.png'
-//                System.out.println(uri);
-//                video.setVideoURI(uri);
-//                video.start();
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception exception) {
-//                // Handle any errors
-//            }
-//        });
-//        l = new ArrayList<>();
-//        storageReference.listAll().addOnSuccessListener(new OnSuccessListener<ListResult>() {
-//            @Override
-//            public void onSuccess(ListResult listResult) {
-//                for (StorageReference fileRef : listResult.getItems()) {
-//                    l.add(fileRef.getPath());
-//                    System.out.println(fileRef.getPath());
-//                }
-//                StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("/uploads/1611424481054.mp4");
-//                Uri file = Uri.fromFile(new File("path/to/folderName/file.jpg"));
-//                UploadTask uploadTask = storageRef.putFile(file);
-//
-//
-//            }
-//        }).addOnFailureListener(new OnFailureListener() {
-//            @Override
-//            public void onFailure(@NonNull Exception e) {
-//                Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
-//            }
-//        });
-//
-//        StorageReference storageRef = FirebaseStorage.getInstance().getReference().child("uploads/1611424481054.mp4");
-//        storageRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
-//            @Override
-//            public void onSuccess(Uri uri) {
-//                video.setVideoURI(uri);
-//                video.start();
-//            }
-//        });
-
-        //video.start();
-
-
     }
 
-    public List<Video> getSpecializedList() {
-        List<Video> specialList = new ArrayList<>();
-        for (int i = 0; i < videoList.size(); i++) {
-            if (videoList.get(i).getCategory().equals(videoCategory)) {
-                specialList.add(videoList.get(i));
-            }
+    private void generateVideo(FirebaseStorage storage) {
+        if (counter >= videoList.size()) {
+            counter = 0;
         }
-        Collections.shuffle(specialList);
-        return specialList;
+        videoPath = videoList.get(counter).getFilePath();
+        counter++;
+        StorageReference storageReference = storage.getReference().child("uploads/" + videoPath);
+        System.out.println("VIDEOPATH " + videoPath);
+        storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                // Got the download URL for 'users/me/profile.png'
+                System.out.println(uri);
+                video.setVideoURI(uri);
+                video.start();
+                //video.pause();
+                for (Video v: videoList) {
+                    System.out.println(v.getCategory());
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle any errors
+                Toast.makeText(getApplicationContext(), "Video Failed", Toast.LENGTH_LONG).show();
+            }
+        });
     }
-
-
 }
